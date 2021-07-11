f945ab4ba9ce90d56f88bb8e76c2be72e8fe94f4
JE-Chen <zenxcvwait@gmail.com>
2021-07-11 18:52:38 +0800

    refactor import & update version
    refactor import  code
    update PYPI version

--- test/import_test.py
+++ test/import_test.py
@@ -1,6 +1,6 @@
 import cv2
 
-from je_open_cv.modules import template_detection
+from je_open_cv import template_detection
 
 image_data_array = template_detection.find_object("../test.png", "../test_template.png")
 
